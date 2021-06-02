470333766eedc882c0dc39743a7191c3a0ca14ff
Chlorie <myosotis@mail.ustc.edu.cn>
2021-06-02 11:42:55 +0800

    Clean things up and test the build again

--- cmake/Findstandardese.cmake
+++
@@ -1,9 +0,0 @@
-find_program(STANDARDESE_EXECUTABLE
-    NAMES standardese
-    DOC "Path to standardese executable")
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(standardese
-    "Failed to find standardese executable"
-    STANDARDESE_EXECUTABLE)
--- docs/index.rst
+++
@@ -1,6 +0,0 @@
-Welcome to Standardese Test Reference!
-====================================
-
-.. toctree::
-   :maxdepth: 1
-   :caption: Contents
