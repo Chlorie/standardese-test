9a80d2bf3d9d72c0b9bb2a5b6444c61ce7c75384
Chlorie <34185634+Chlorie@users.noreply.github.com>
2022-09-26 15:53:50 +0800

    Test

--- lib/include/test/test.h
+++ lib/include/test/test.h
@@ -8,7 +8,7 @@ namespace test
     /// \returns The sum of the two integers.
     [[nodiscard]] constexpr int add(const int a, const int b) noexcept { return a + b; }
 
-    /// Calculates the square of an integer.
+    /// Calculates the square of an integer. See also [test::add]()
     /// \param a The integer.
     /// \returns The square of the input.
     [[nodiscard]] constexpr int square(const int a) noexcept { return a * a; }
